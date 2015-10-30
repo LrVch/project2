b32ca044c134444854ea9e49572f92199c156569
LrVch <caden45@mail.ru>
2015-10-30 18:50:56 +0600

    sort: добавил проверку вызова

--- app/js/sort.js
+++ app/js/sort.js
@@ -1,48 +1,48 @@
-var sort = (function () {
+(function () {
 
-  function init() {
-    _setUpListners();
-    _modules();
-  };
+  var sort = (function () {
 
-  function _setUpListners() {
-    $('.sort__view-link').on('click', _sort);
-  };
+    function init() {
+      _setUpListners();
+    };
 
-  function _modules() {
+    function _setUpListners() {
+      $('.sort__view-link').on('click', _sort);
+    };
 
-  };
+    function _sort(e) {
+      e.preventDefault();
+      var
+        $this = $(this),
+        list = $this.closest(".sort__view-list"),
+        links = list.find(".sort__view-link"),
+        goods = $(".mobile");
 
-  function _sort(e) {
-    e.preventDefault();
-    var
-      $this = $(this),
-      list = $this.closest(".sort__view-list"),
-      links = list.find(".sort__view-link"),
-      goods = $(".mobile");
+      links.removeClass("sort__view-link--active");
+      $this.addClass("sort__view-link--active");
 
-    links.removeClass("sort__view-link--active");
-    $this.addClass("sort__view-link--active");
+      if ($this.hasClass("sort__view-link--row")) {
+        goods.removeClass("mobile__grid mobile__list");
+        goods.addClass("mobile__row")
+      }
+      if ($this.hasClass("sort__view-link--grid")) {
+        goods.removeClass("mobile__row mobile__list");
+        goods.addClass("mobile__grid")
+      }
+      if ($this.hasClass("sort__view-link--list")) {
+        goods.removeClass("mobile__grid mobile__row");
+        goods.addClass("mobile__list")
+      }
+    };
 
-    if ($this.hasClass("sort__view-link--row")) {
-      goods.removeClass("mobile__grid mobile__list");
-      goods.addClass("mobile__row")
-    }
-    if ($this.hasClass("sort__view-link--grid")) {
-      goods.removeClass("mobile__row mobile__list");
-      goods.addClass("mobile__grid")
-    }
-    if ($this.hasClass("sort__view-link--list")) {
-      goods.removeClass("mobile__grid mobile__row");
-      goods.addClass("mobile__list")
-    }
-  };
+    return {
+      init: init
+    };
 
-  return {
-    init: init
-  };
+  })();
 
-})();
+  if ($('.sort__view-link').length) {
+    sort.init();
+  }
 
-// Вызов модуля
-sort.init();
+}());
