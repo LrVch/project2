ec35d97d3d64fc9109ff278567bf7e5b3bcfc012
LrVch <caden45@mail.ru>
2015-10-30 16:16:36 +0600

    columnize: сделал проверку

--- app/js/columnize.js
+++ app/js/columnize.js
@@ -1,6 +1,9 @@
-"use strict";
 (function () {
-  $('#info-block').columnize({
-    width: 535
-  });
+  "use strict";
+  if ($("#info-block").length) {
+
+    $('#info-block').columnize({
+      width: 535
+    });
+  }
 })();
