401d583c723d7af7adc0dbd3322a82b3686d9e2b
LrVch <caden45@mail.ru>
2015-10-30 18:48:10 +0600

    slideshow: добавил проверку вызова

--- app/js/slideshow.js
+++ app/js/slideshow.js
@@ -1,43 +1,52 @@
-var slideShow = (function () {
+(function () {
 
-  function init() {
-    _setUpListners();
-  };
 
-  function _setUpListners() {
-    $('.foto__preview-item').on('mouseenter', _slideShow);
-    $('.foto__preview-item').on('click', _prevent);
-  };
 
-  function _slideShow(e) {
-    e.preventDefault();
-    var
-      $this = $(this),
-      img = $this.find("img"),
-      container = $this.closest(".foto"),
-      display = container.find(".foto__main"),
-      path = $this.find("img").attr("src"),
-      duration = 300;
-    //console.log(display);
+  var slideShow = (function () {
 
-    if (!$this.hasClass("active")) {
-      $this.addClass("active").siblings().removeClass("active");
-      display.find("img").fadeOut(duration, function () {
-        $(this).attr("src", path).fadeIn(duration);
-        console.log(img);
-      });
+    function init() {
+      _setUpListners();
+    };
+
+    function _setUpListners() {
+      $('.foto__preview-item').on('mouseenter', _slideShow);
+      $('.foto__preview-item').on('click', _prevent);
+    };
+
+    function _slideShow(e) {
+      e.preventDefault();
+      var
+        $this = $(this),
+        img = $this.find("img"),
+        container = $this.closest(".foto"),
+        display = container.find(".foto__main"),
+        path = $this.find("img").attr("src"),
+        duration = 300;
+      //console.log(display);
+
+      if (!$this.hasClass("active")) {
+        $this.addClass("active").siblings().removeClass("active");
+        display.find("img").fadeOut(duration, function () {
+          $(this).attr("src", path).fadeIn(duration);
+          //console.log(img);
+        });
+      }
+
+    };
+
+    function _prevent(e) {
+      e.preventDefault();
     }
 
-  };
-  function _prevent(e) {
-    e.preventDefault();
+    return {
+      init: init
+    };
+
+  })();
+
+  if ($(".foto").length) {
+    slideShow.init();
   }
 
-  return {
-    init: init
-  };
 
-})();
-
-// Вызов модуля
-slideShow.init();
+}());
